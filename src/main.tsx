6ae18f62f51fc86a01bf496662cf63df8ee71976
Avior Kahalani <59415129+AviorKahalani@users.noreply.github.com>
2022-12-22 18:06:36 +0200

    fix: remove unneeded line

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import './sass/main.scss'
 import { router } from './router'
 import { Provider } from 'react-redux'
 import { store } from './store'
-// import { store } from './state'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
